98265a0edb272c2ceda62d85114271ea0adc8960
Chunhui Liu <leeychee@gmail.com>
2017-06-03 20:11:54 +0800

    add 'strange_counter'

---
+++ practice/algorithms/implementation/strange_counter/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	t := readInput(os.Stdin)
+	fmt.Println(counter2(t))
+}
+
+func readInput(r io.Reader) (t int) {
+	fmt.Fscanf(r, "%d\n", &t)
+	return
+}
+
+var start = 3
+
+type pair struct{ time, value int }
+
+func (p *pair) String() string {
+	return fmt.Sprintf("[%d, %d]", p.time, p.value)
+}
+
+func counter() <-chan *pair {
+	c := make(chan *pair)
+	go func() {
+		curMark := start
+		t := 1
+		curT := t
+		var v int
+		for {
+			v = curMark - curT + 1
+			c <- &pair{t, v}
+			t++
+			if v == 1 {
+				curMark *= 2
+				curT = 1
+			} else {
+				curT++
+			}
+		}
+	}()
+	return c
+}
+
+func counter2(t int) int {
+	curMark := start
+	for t > curMark {
+		t -= curMark
+		curMark *= 2
+	}
+	return curMark - t + 1
+}
+
+func counter3() <-chan int {
+	c := make(chan int)
+	go func() {
+		t := 1
+		for {
+			c <- counter2(t)
+			t++
+		}
+	}()
+	return c
+}
+
+func counter4() <-chan *pair {
+	c := make(chan *pair)
+	go func() {
+		t := 1
+		for {
+			c <- &pair{t, counter2(t)}
+			t++
+		}
+	}()
+	return c
+}
---
+++ practice/algorithms/implementation/strange_counter/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import "testing"
+
+func TestStrangeCounter(t *testing.T) {
+	var cases = []struct {
+		t, e int
+	}{
+		{1, 3},
+		{3, 1},
+		{4, 6},
+		{7, 3},
+		{14, 8},
+		{15, 7},
+		{21, 1},
+	}
+	for _, c := range cases {
+		g := counter2(c.t)
+		if g != c.e {
+			t.Errorf("%d, Expected: %d, Got: %d\n", c.t, c.e, g)
+		}
+	}
+}
+
+func TestCounter(t *testing.T) {
+	pc := counter()
+	for i := 0; i < 21; i++ {
+		p := <-pc
+		t.Logf("%s", p)
+	}
+}
+
+func BenchmarkCounter(b *testing.B) {
+	pc := counter()
+	for i := 0; i < b.N; i++ {
+		<-pc
+	}
+}
+
+func BenchmarkCounter2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		counter2(i)
+	}
+}
+
+func BenchmarkCounter3(b *testing.B) {
+	pc := counter3()
+	for i := 0; i < b.N; i++ {
+		<-pc
+	}
+}
+
+func BenchmarkCounter4(b *testing.B) {
+	pc := counter4()
+	for i := 0; i < b.N; i++ {
+		<-pc
+	}
+}
