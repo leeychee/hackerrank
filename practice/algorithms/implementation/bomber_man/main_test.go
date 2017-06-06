7c5a7d3f29f66737fe552bf2e1f57dca2c6792d5
Chunhui Liu <leeychee@gmail.com>
2017-06-06 22:40:02 +0800

    add 'bomber man' test case

--- practice/algorithms/implementation/bomber_man/main_test.go
+++ practice/algorithms/implementation/bomber_man/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import "testing"
 import "bytes"
+import "os"
 
 func TestAfter(t *testing.T) {
 	var cases = []struct {
@@ -31,5 +32,35 @@ OOO...O
 		if g0.deepEqual(g1) {
 			t.Errorf("%s\nAfter %d, Expected:\n%s, Got:\n%s", c.ogs, c.n, g1, g0)
 		}
+	}
+}
+
+func TestAfterFromFile(t *testing.T) {
+	var cases = []struct {
+		fis, fos string
+	}{
+		{"input13.txt", "output13.txt"},
+		{"input24.txt", "output24.txt"},
+	}
+	for _, c := range cases {
+		fi, _ := os.Open(c.fis)
+		g, n := readInput(fi)
+		g.after(n)
+		e := newGrid(g.r, g.c)
+		fo, _ := os.Open(c.fos)
+		e.read(fo)
+		if !g.equal(e) {
+			t.Errorf("Test case %s failed.", c.fis)
+		}
+	}
+}
+
+func BenchmarkAfter(b *testing.B) {
+	fin, _ := os.Open("./input24.txt")
+	g, n := readInput(fin)
+	g0 := g.deepCopy()
+	for i := 0; i < b.N; i++ {
+		g.after(n)
+		g = g0.deepCopy()
 	}
 }
