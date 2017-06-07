75d36389dd67bd9b2d7c4059a3aab07ca54122d6
Chunhui Liu <leeychee@gmail.com>
2017-06-07 17:50:07 +0800

    make 'ama supercomputer' works.

--- practice/algorithms/implementation/ama_s_supercomputer/main.go
+++ practice/algorithms/implementation/ama_s_supercomputer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -8,12 +9,8 @@ import (
 
 func main() {
 	g := readInput(os.Stdin)
-	pluses := g.validPluses()
-	if len(pluses) >= 2 {
-		area := len(pluses[0]) * len(pluses[1])
-		fmt.Println(area)
-	}
-	fmt.Println(0)
+	maxArea := g.maxGoodArea()
+	fmt.Println(maxArea)
 }
 
 func readInput(rd io.Reader) grid {
@@ -22,16 +19,122 @@ func readInput(rd io.Reader) grid {
 	g := make(grid, r)
 	for i := 0; i < r; i++ {
 		var s string
-		fmt.Fscanf(rd, fmt.Sprintf("%%%ds", c), &s)
+		fmt.Fscanf(rd, fmt.Sprintf("%%%ds\n", c), &s)
 		g[i] = []byte(s)
 	}
 	return g
 }
 
-type grid [][]byte
-type plus []cell
 type cell struct{ i, j int }
+type grid [][]byte
 
-func (g grid) validPluses() []plus {
+type plus struct {
+	center cell
+	_type  byte
+	d      int
+}
+
+func (p *plus) String() string {
+	return fmt.Sprintf("([%d, %d]: %d)", p.center.i, p.center.j, p.d)
+}
+
+func (p *plus) i() int   { return p.center.i }
+func (p *plus) j() int   { return p.center.j }
+func (p *plus) len() int { return 4*p.d + 1 }
+
+func (p *plus) overlap(p0 *plus) bool {
+	m := make(map[cell]struct{})
+	for i := p.i() - p.d; i <= p.i()+p.d; i++ {
+		m[cell{i, p.j()}] = struct{}{}
+	}
+	for j := p.j() - p.d; j <= p.j()+p.d; j++ {
+		m[cell{p.i(), j}] = struct{}{}
+	}
+	for i := p0.i() - p0.d; i <= p0.i()+p0.d; i++ {
+		if _, ok := m[cell{i, p0.j()}]; ok {
+			return true
+		}
+	}
+	for j := p0.j() - p0.d; j <= p0.j()+p0.d; j++ {
+		if _, ok := m[cell{p0.i(), j}]; ok {
+			return true
+		}
+	}
+	return false
+}
+
+var errUnableExpand = errors.New("unable expand")
+
+func (p *plus) expand(g grid) error {
+	d0 := p.d + 1
+	i, j := p.center.i, p.center.j
+	if !g.valid(i+d0, j) || g[i+d0][j] != p._type {
+		return errUnableExpand
+	}
+	if !g.valid(i-d0, j) || g[i-d0][j] != p._type {
+		return errUnableExpand
+	}
+	if !g.valid(i, j-d0) || g[i][j-d0] != p._type {
+		return errUnableExpand
+	}
+	if !g.valid(i, j+d0) || g[i][j+d0] != p._type {
+		return errUnableExpand
+	}
+	p.d++
 	return nil
+}
+
+func (g grid) r() int {
+	return len(g)
+}
+
+func (g grid) c() int {
+	return len(g[0])
+}
+
+func (g grid) valid(i, j int) bool {
+	return i >= 0 && i < g.r() && j >= 0 && j < g.c()
+}
+
+func (g grid) maxGoodArea() int {
+	var ps []*plus
+	var pm = make(map[string]struct{})
+	for i := 0; i < g.r(); i++ {
+		for j := 0; j < g.c(); j++ {
+			if g[i][j] != 'G' {
+				continue
+			}
+			c := cell{i, j}
+			p := &plus{center: c, _type: 'G'}
+			for k := 1; ; k++ {
+				if _, ok := pm[p.String()]; !ok {
+					pm[p.String()] = struct{}{}
+					// copy the plus object.
+					// assignment makes copy.
+					p1 := *p
+					ps = append(ps, &p1)
+				}
+				if err := p.expand(g); err != nil {
+					break
+				}
+			}
+		}
+	}
+	// for i := 0; i < len(ps); i++ {
+	// 	fmt.Printf("%s\n", ps[i])
+	// }
+	// fmt.Println("------------------------------")
+	max := 0
+	for i := 0; i < len(ps)-1; i++ {
+		for j := i + 1; j < len(ps); j++ {
+			if !ps[i].overlap(ps[j]) {
+				a := ps[i].len() * ps[j].len()
+				// fmt.Printf("%s %s, %d*%d=%d\n", ps[i], ps[j], ps[i].len(), ps[j].len(), a)
+				if a > max {
+					max = a
+				}
+			}
+		}
+	}
+	return max
 }
--- practice/algorithms/implementation/ama_s_supercomputer/main_test.go
+++ practice/algorithms/implementation/ama_s_supercomputer/main_test.go
@@ -2,32 +2,66 @@ package main
 
 import "testing"
 import "bytes"
-import "reflect"
 
-func TestGridPluses(t *testing.T) {
+func TestOverlap(t *testing.T) {
+	var cases = []struct {
+		p0, p1 *plus
+		e      bool
+	}{
+		{&plus{center: cell{1, 1}}, &plus{center: cell{1, 1}}, true},
+		{&plus{center: cell{1, 1}}, &plus{center: cell{2, 1}}, false},
+		{&plus{center: cell{1, 2}, d: 1}, &plus{center: cell{2, 1}, d: 1}, true},
+		{&plus{center: cell{1, 1}, d: 1}, &plus{center: cell{4, 3}, d: 1}, false},
+		{&plus{center: cell{1, 1}, d: 1}, &plus{center: cell{1, 3}, d: 1}, true},
+		{&plus{center: cell{1, 1}, d: 1}, &plus{center: cell{2, 3}, d: 1}, false},
+		{&plus{center: cell{2, 2}, d: 2}, &plus{center: cell{3, 3}, d: 2}, true},
+	}
+	for _, c := range cases {
+		g := c.p0.overlap(c.p1)
+		if g != c.e {
+			t.Errorf("%s, %s, Expected: %t, Got: %t", c.p0, c.p1, c.e, g)
+		}
+	}
+}
+
+func TestMaxGoodArea(t *testing.T) {
 	var cases = []struct {
 		s string
-		e []plus
+		e int
 	}{
 		{`5 6
 GGGGGG
 GBBBGB
 GGGGGG
 GGBBGB
-GGGGGG`, []plus{{{2, 1}}, {{1, 4}, {2, 3}, {2, 4}, {2, 5}, {3, 4}}}},
+GGGGGG`, 5},
 		{`6 6
 BGBBGB
 GGGGGG
 BGBBGB
 GGGGGG
 BGBBGB
-BGBBGB`, []plus{{{0, 1}, {1, 0}, {1, 1}, {1, 2}, {2, 1}}, {{2, 4}, {3, 3}, {3, 4}, {3, 5}, {4, 4}}}},
+BGBBGB`, 25},
+		{`12 12
+GGGGGGGGGGGG
+GBGGBBBBBBBG
+GBGGBBBBBBBG
+GGGGGGGGGGGG
+GGGGGGGGGGGG
+GGGGGGGGGGGG
+GGGGGGGGGGGG
+GBGGBBBBBBBG
+GBGGBBBBBBBG
+GBGGBBBBBBBG
+GGGGGGGGGGGG
+GBGGBBBBBBBG`, 81},
 	}
 	for _, c := range cases {
-		g := readInput(bytes.NewReader([]byte(c.s)))
-		p := g.validPluses()
-		if !reflect.DeepEqual(p, c.e) {
-			t.Errorf("Expected: %v, Got: %v", c.e, p)
+		grid := readInput(bytes.NewReader([]byte(c.s)))
+		g := grid.maxGoodArea()
+		if g != c.e {
+			t.Errorf("%s\n, Expected: %d, Got: %d", c.s, c.e, g)
 		}
 	}
+
 }
