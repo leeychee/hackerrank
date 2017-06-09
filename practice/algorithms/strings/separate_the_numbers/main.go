22f674ece4a6de752a122c400c0af012f925346b
Chunhui Liu <leeychee@gmail.com>
2017-06-09 10:24:06 +0800

    make 'separate the number' works

--- practice/algorithms/strings/separate_the_numbers/main.go
+++ practice/algorithms/strings/separate_the_numbers/main.go
@@ -1,5 +1,67 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"strconv"
+)
 
+func main() {
+	ss := readInput(os.Stdin)
+	for _, s := range ss {
+		bs, err := beautiful(s)
+		if err == nil {
+			fmt.Printf("YES %d\n", bs[0])
+		} else {
+			fmt.Println("NO")
+		}
+	}
+}
+
+func readInput(r io.Reader) []string {
+	var n int
+	fmt.Fscanf(r, "%d", &n)
+	ss := make([]string, n)
+	for i := range ss {
+		fmt.Fscanf(r, "%s\n", &ss[i])
+	}
+	return ss
+}
+
+var errNotBeautiful = errors.New("string not beautiful")
+
+func beautiful(s string) ([]int, error) {
+	bs := []byte(s)
+	for i := 1; i <= len(bs)/2; i++ {
+		var ss []int
+		j := 0
+		curL := i
+		for curL+j < len(bs) {
+			c0, err := strconv.Atoi(string(bs[j : curL+j]))
+			if err != nil {
+				return nil, err
+			}
+			c1s := strconv.Itoa(c0 + 1)
+			if curL+j+len(c1s) > len(bs) {
+				break
+			}
+			if !reflect.DeepEqual([]byte(c1s), bs[curL+j:curL+j+len(c1s)]) {
+				break
+			} else {
+				ss = append(ss, c0)
+				j = j + curL
+				curL = len(c1s)
+				if j+curL == len(bs) {
+					ss = append(ss, c0+1)
+					return ss, nil
+				} else if j+curL > len(bs) {
+					break
+				}
+			}
+		}
+	}
+	return nil, errNotBeautiful
 }
--- practice/algorithms/strings/separate_the_numbers/main_test.go
+++ practice/algorithms/strings/separate_the_numbers/main_test.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBeautiful(t *testing.T) {
+	var cases = []struct {
+		s string
+		b []int
+		e error
+	}{
+		{"1234", []int{1, 2, 3, 4}, nil},
+		{"0102", nil, errNotBeautiful},
+		{"91011", []int{9, 10, 11}, nil},
+		{"99100", []int{99, 100}, nil},
+		{"9991000", []int{999, 1000}, nil},
+		{"99910000", nil, errNotBeautiful},
+		{"989910010110", nil, errNotBeautiful},
+		{"101103", nil, errNotBeautiful},
+		{"010203", nil, errNotBeautiful},
+		{"13", nil, errNotBeautiful},
+		{"1", nil, errNotBeautiful},
+	}
+	for _, c := range cases {
+		gb, ge := beautiful(c.s)
+		if ge != c.e || !reflect.DeepEqual(gb, c.b) {
+			t.Errorf("%q, Expected: %v, Got: %v", c.s, c.b, gb)
+		}
+	}
+}
